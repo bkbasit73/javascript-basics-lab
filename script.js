bfc3ef9145fc6c72ced3a3e3f59d9f4a1f64e034
bkbasit73 <bkbasit73@gmail.com>
2024-09-19 15:00:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//Declare and Use Variables
+let studentName = "Mark";
+let studentAge = 23;
+let isEnrolled = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+
+//Demonstrate Data Types
+let stringValue = "Hello, World!";
+let numericValue = 42;
+let booleanValue = false;
+let fruits = ["apple", "banana", "cherry"];
+let person = { firstName: "mark", lastName: "angel" };
+
+
+console.log("String:", stringValue);
+console.log("Number:", numericValue);
+console.log("Boolean:", booleanValue);
+console.log("Array:", fruits);
+console.log("Object:", person);
+
+// Use basic operators
+let x = 8;
+let y = 5;
+
+console.log("Addition:", x + y);
+console.log("Subtraction:", x - y);
+console.log("Multiplication:", x * y);
+console.log("Division:", x / y);
+
+console.log("Greater than:", x > y);
+console.log("Less than:", x < y);
+console.log("Equal to:", x === y);
+console.log("Not equal:", x !== y);
+
+console.log("AND:", true && true);
+console.log("OR:", true || false);
+console.log("NOT:", !true);
